de15d47f6ade386b4e7d6c578007f8fabab50ca7
Daniel Mota <danixmotaa7x@gmail.com>
2023-01-15 22:22:19 -0300

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Bijuteria Site</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,8 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <head>
-        <title>Bijuteria Site</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
-        />
-        <Script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></Script>
-        <Script type="text/javascript" src="js/script.js"></Script>
-      </head>
+      <Script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></Script>
+      <Script type="text/javascript" src="js/script.js"></Script>
       <header className="header">
         <a href="#" className="logo">
           <i className="far fa-gem"> accessory company </i>
